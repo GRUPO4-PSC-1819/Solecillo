a2a135257a5bfe0b1e07c92801e97eae41c4a9e8
Echever <imanolecheverria13@gmail.com>
2018-10-08 13:05:40 +0200

    ssssss

--- Solecillo/src/main/java/Grupo4/Solecillo/App.java
+++ Solecillo/src/main/java/Grupo4/Solecillo/App.java
@@ -11,6 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("fefev");
         System.out.println("Josune");
+        System.out.println("IManol");
         
     }
 }
