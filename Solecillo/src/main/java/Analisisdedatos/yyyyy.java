9f934fff2e56eb695f3d86c21e2d1f0475f2f128
Echever <imanolecheverria13@gmail.com>
2018-10-15 10:57:13 +0200

    ppp #PROYGRUPO4PSC1819-23 aaaa

--- Solecillo/src/main/java/Analisisdedatos/yyyyy.java
+++ Solecillo/src/main/java/Analisisdedatos/yyyyy.java
@@ -23,7 +23,7 @@ public class yyyyy {
 	public static void main(String[] args) {
 		
 		// TODO Auto-generated method stub
-		System.out.println("ffffff");
+		System.out.println("fffffff");
 		ArrayList<Attribute> atts = new ArrayList<Attribute>(3);
 		ArrayList<String> classVal = new ArrayList<String>();
         classVal.add("No peligra");
