90df540ffff5ec70b64248cf38359f0426b1b2de
John DoeGarikoitz <gari.bereciartua@opendeusto.es>
2018-11-23 17:33:33 +0100

    subo algo de JavaDoc que he empezado porque me tengo que ir

--- Solecillo/src/main/java/LN/clsCliente.java
+++ Solecillo/src/main/java/LN/clsCliente.java
@@ -2,11 +2,10 @@ package LN;
 
 import java.io.Serializable;
 
-
 /**
- * Clase creada para generar un objeto nuevo (clsCliente). Implementa la interfaz Comparable con clsUsuario y Serializable. <br>
- * La ordenaciÃ³n natural se ha hecho mediante los nicknames. Se implementarÃ¡n de manera externa otros tipos de ordenaciones.
- * @author Garikoitz Bereciartua (garibere13), Imanol Echeverria (Echever), Josune OrdoÃ±ez (Josune07)
+ * Clase creada para generar un objeto nuevo (clsCliente). Implementa la interfaz Comparable con clsCliente y Serializable. <br>
+ * La ordenación natural se ha hecho mediante los dni.
+ * @author Garikoitz Bereciartua (garibere13), Imanol Echeverria (Echever), Josune Ordoñez (Josune07)
  */
 
 public class clsCliente implements Serializable, Comparable<clsCliente> 
@@ -38,12 +37,12 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 	}
 
 	/**
-	 * Constructor con parÃ¡metros para crear un nuevo usuario.
+	 * Constructor con parámetros para crear un nuevo cliente.
 	 * @param n Nombre del cliente
 	 * @param ap1 Primer apellido del cliente
 	 * @param ap2 Segundo apellido del cliente
-	 * @param nick Nickname del cliente
-	 * @param cont ContraseÃ±a del cliente
+	 * @param dni DNI del cliente
+	 * @param emp Empresa del cliente
 	 */
 	public clsCliente(String n, String ap1, String ap2, String dni, String emp)
 	{
@@ -54,6 +53,11 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 		setEmpresa(emp);
 	}
 	
+	/**
+	 * Constructor dos parámetros para crear un nuevo cliente.
+	 * @param dni DNI del cliente
+	 * @param tot_ventas Total de Ventas hechas a ese cliente
+	 */
 	public clsCliente(String dni, int tot_ventas)
 	{
 		this.dni=dni;
@@ -61,18 +65,7 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 	}
 	
 	/**
-	 * Constructor con parÃ¡metros para tomar usuarios de la Base de Datos.
-	 * @param n Nombre del usuario
-	 * @param ap1 Primer apellido del usuario
-	 * @param ap2 Segundo apellido del usuario
-	 * @param nick Nickname del usuario
-	 * @param cont ContraseÃ±a del usuario
-	 * @param el PuntuaciÃ³n ELO del usuario
-	 * @param fecha Fecha de alta del usuario
-	 */
-	
-	/**
-	 * Constructor vacÃ­o para poder serializar.
+	 * Constructor vacío.
 	 */
 	public clsCliente()
 	{
@@ -81,7 +74,6 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 		apellido2=null;
 		dni=null;
 		setEmpresa(null);
-
 	}
 	
 	@Override
@@ -95,7 +87,7 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 
 
 	/**
-	 * ReimplementaciÃ³n del mÃ©todo toString.
+	 * Reimplementación del método toString.
 	 */
 	public String toString()
 	{
@@ -103,11 +95,6 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 				" - DNI: "+this.getdni();
 		return e;
 	}
-	
-	/**
-	 * OrdenaciÃ³n natural hecha mediante nickname de usuarios.
-	 */
-	
 
 	public String getNombre() 
 	{
@@ -156,9 +143,4 @@ public class clsCliente implements Serializable, Comparable<clsCliente>
 	public void setTot_Ventas(int tot_ventas) {
 		this.tot_ventas = tot_ventas;
 	}
-
 }
-	
-	
-	
-
