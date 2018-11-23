1ae60a23c1d35c54f7010a01fa9688e2a309f202
garibere13 <gari.bereciartua@opendeusto.es>
2018-11-23 23:05:53 +0100

    Delete class_grupo4_1_1_solecillo_1_1_app_test.js

--- class_grupo4_1_1_solecillo_1_1_app_test.js
+++
@@ -1,7 +0,0 @@
-var class_grupo4_1_1_solecillo_1_1_app_test =
-[
-    [ "testAlta", "class_grupo4_1_1_solecillo_1_1_app_test.html#a64ccde0697e19dc1d3003cb1c3857516", null ],
-    [ "testBD", "class_grupo4_1_1_solecillo_1_1_app_test.html#ae2c6f222b319dac57120b5504eccca12", null ],
-    [ "testGraficos", "class_grupo4_1_1_solecillo_1_1_app_test.html#a448da09e2ea5de76ac0d91dbc948fc78", null ],
-    [ "testLN", "class_grupo4_1_1_solecillo_1_1_app_test.html#ac175d555c6c17b5e1fc6e741dd52c101", null ]
-];
