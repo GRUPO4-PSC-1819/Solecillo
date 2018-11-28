b4b73421e1b4fe40f5afb79328261b43f743873e
John DoeGarikoitz <gari.bereciartua@opendeusto.es>
2018-11-28 12:16:25 +0100

    Tarea de ánalisis de datos y generación de informes acabada #PROYGRUPO4PSC1819-43 Done work 16

--- Solecillo/src/test/java/Analisisdedatos/Graficos_Test.java
+++ Solecillo/src/test/java/Analisisdedatos/Graficos_Test.java
@@ -9,12 +9,19 @@ public class Graficos_Test {
 	@Test
 	public void test() {
 		Pueblo_Altura_Diametro p=new Pueblo_Altura_Diametro("", "");
+		p.setVisible(true);
 		Rio_Fabricante_Maquina pq=new Rio_Fabricante_Maquina("", "");
+		pq.setVisible(true);
 		ScatterPlotExample example = new ScatterPlotExample("Scatter Chart Example | BORAJI.COM");
+		example.setVisible(true);
 		top_clientes jn=new top_clientes("");
+		jn.setVisible(true);
 		Valor_Estado vg=new Valor_Estado("");
+		vg.setVisible(true);
 		Valor_Medio_Maquinas t =new Valor_Medio_Maquinas("", "");
+		t.setVisible(true);
 		Ventas_Maquina fve=new Ventas_Maquina("");
+		fve.setVisible(true);
 	}
 
 }
