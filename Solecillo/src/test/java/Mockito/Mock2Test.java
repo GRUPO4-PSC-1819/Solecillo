6b8bc250818dc819197e11394681dfb213c88173
Echever <imanolecheverria13@gmail.com>
2018-11-30 18:22:41 +0100

    Update Mock2Test.java

--- Solecillo/src/test/java/Mockito/Mock2Test.java
+++ Solecillo/src/test/java/Mockito/Mock2Test.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import Analisisdedatos.top_clientes;
+import LN.IGestor;
 import LN.clsCliente;
 import LN.clsGestor;
 
@@ -18,25 +19,27 @@ public class Mock2Test {
 	clsGestor ges;
 	@Before
 	public void setup(){
-		 ges=Mockito.mock(clsGestor.class);
 		ArrayList<clsCliente> a=new ArrayList<clsCliente>();
 		a.add(new clsCliente("a",50));
 		a.add(new clsCliente("b",40));
 		a.add(new clsCliente("c",30));
 		a.add(new clsCliente("d",20));
 		a.add(new clsCliente("f",10));
-		Mockito.when(ges.top_5_clientes()).thenReturn(a);
+		IGestor gestor = Mockito.mock(IGestor.class);
+		Mockito.when(gestor.top5()).thenReturn(a);
+		ges=new clsGestor();
+		ges.setI(gestor);
 	}
 	
 	@Test
 	public void test() {
-		top_clientes demo = new top_clientes( "Ventas" ,ges.top_5_clientes());  
+		top_clientes demo = new top_clientes( "Ventas" ,ges.top5_clientes());  
 	      demo.setSize( 560 , 367 );    
 	      RefineryUtilities.centerFrameOnScreen( demo );   
 	      demo.pack();
 	      demo.setVisible( true );
 	      try {
-			Thread.sleep(10000);
+			Thread.sleep(2000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
